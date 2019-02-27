55a9eceec42e672beece7ef11222c922f873221c
Jameson13B <jamesonbrown@neolivity.com>
2019-02-27 11:39:23 -0700

    Created basic layout and styling of the application

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      joke: "",
+      theme: "day"
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">Dad Joke Generator</header>
+        <header className="App-content">
+          <div className="top-container">
+            {this.state.joke ? this.state.joke : "Joke goes here..."}
+          </div>
+          <div className="left-container btn">NEW</div>
+          <div className="right-container btn">COPY</div>
+        </header>
       </div>
     );
   }
