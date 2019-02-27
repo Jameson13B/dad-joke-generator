8b69fbadadea909c1755eed9e4cb3bc8fe3864d0
Jameson13B <jamesonbrown@neolivity.com>
2019-02-27 12:41:59 -0700

    Added axios package and functionality for api requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
@@ -9,14 +10,39 @@ class App extends Component {
       theme: "day"
     };
   }
+  componentDidMount() {
+    this.handleJoke();
+  }
+  handleJoke = () => {
+    const url = "https://icanhazdadjoke.com/";
+    const config = {
+      headers: {
+        Accept: "application/json",
+        // Requested by API for tracking
+        "User-Agent": "https://jamesonb.com/"
+      }
+    };
+    axios
+      .get(url, config)
+      .then(res => {
+        this.setState({
+          joke: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   render() {
     return (
       <div className="App">
         <header className="App-content">
           <div className="top-container">
-            {this.state.joke ? this.state.joke : "Joke goes here..."}
+            {this.state.joke ? this.state.joke.joke : "Joke goes here..."}
           </div>
-          <div className="left-container btn">NEW</div>
+          <div className="left-container btn" onClick={this.handleJoke}>
+            NEW
+          </div>
           <div className="right-container btn">COPY</div>
         </header>
       </div>
